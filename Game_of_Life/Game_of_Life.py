ecb8c760d26d7c8197f5eb594c3f775e3a55ea56
Alesha-student <153225397+Alesha-student@users.noreply.github.com>
2023-12-14 23:54:48 +0300

    К замечанию 3
    Поле сделал бесконечным, добавил шаблон в виде креста (активный) и диагональный

--- Game_of_Life/Game_of_Life.py
+++ Game_of_Life/Game_of_Life.py
@@ -13,15 +13,21 @@ clock = pg.time.Clock()
 
 next_blocks_stage = [[0 for l in range(width_count)] for j in range(height_count)]
 
-blocks = [[randint(0, 1) for l in range(width_count)] for j in range(height_count)]
+#blocks = [[randint(0, 1) for l in range(width_count)] for j in range(height_count)]#Random pattern
 
+blocks = [[1 if i == width_count // 2 or  (j == height_count // 2) else 0 for i in range(width_count)] for j in range(height_count)]#Cross pattern
+
+'''blocks = [[0 for i in range(width_count)] for j in range(height_count)]
+for i in range(height_count):
+    blocks[i][i+(width_count-height_count) // 2]=1
+    blocks[height_count-i-1][i+(width_count-height_count) // 2]=1'''#Diogonal pattern
 
 def check_block(field, pos):
     x, y = pos
     neighbors = 0
     for yS in range(y-1, y+2):
         for xS in range(x-1, x+2):
-            if field[yS][xS] == 1:
+            if field[yS % height_count][xS % width_count] == 1:
                 neighbors += 1
 
     if field[y][x] == 1:
@@ -47,8 +53,8 @@ while True:
     [pg.draw.line(screen, (78,78,78), (x,0), (x, height)) for x in range(0, width, size)]
     [pg.draw.line(screen, (78,78,78), (0,y), (width, y)) for y in range(0, height, size)]
 
-    for x_block in range(width_count-1):
-        for y_block in range(height_count-1):
+    for x_block in range(width_count):
+        for y_block in range(height_count):
             if blocks[y_block][x_block] == 1:
                 pg.draw.rect(screen, (255,255,255), (x_block * size + 2, y_block * size + 2, size-2, size-2))
             next_blocks_stage[y_block][x_block] = check_block(blocks, (x_block, y_block))
